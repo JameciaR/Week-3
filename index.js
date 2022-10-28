d0752eeae896ce0451bb6a13f9563af64d32a677
Jamecia Reed <jameciareed@Jamecias-Air.lan>
2022-10-28 09:53:56 -0500

    added coding assignment

---
+++ index.js
@@ -0,0 +1,29 @@
+  //*4 Inside index.js create one variable for each of the following real-life examples 
+  let itemPrice = 20 ;
+  let amountOfMoneyInWallet = 100;
+  let friendsNumber = 2;
+  let ageInYears = 30;
+  let firstName = "Jamecia";
+  let lastName = "Reed";
+  let middleInitial = "D";
+
+  
+  //* 5 Create the following variables by performing the operations
+  let moneyInWallet = amountOfMoneyInWallet - 20;
+  console.log(amountOfMoneyInWallet - itemPrice);
+  let friends = ageInYears + friendsNumber;
+  console.log(ageInYears + friendsNumber);
+  let name = firstName + " " + middleInitial + " " + lastName;
+  console.log(name);
+
+  //* 6 Use console.log() to print values of all the codes you created
+  console.log(itemPrice);
+  console.log(amountOfMoneyInWallet);
+  console.log(friendsNumber);
+  console.log(ageInYears);
+  console.log(firstName);
+  console.log(middleInitial);
+  console.log(lastName);
+  
+
+
